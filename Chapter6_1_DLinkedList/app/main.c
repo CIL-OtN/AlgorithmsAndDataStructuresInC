6d82eacde8cfde8579101fe1179ed98b2616d8c8
Ika-OtN <schneiderwaldemar@outlook.de>
2021-07-09 09:24:47 +0200

    update doublylinkedlist

--- Chapter6_1_DLinkedList/app/main.c
+++ Chapter6_1_DLinkedList/app/main.c
@@ -13,28 +13,6 @@ typedef struct node
     struct node* next;
 } node_t;
 
-void printForward(node_t** head) 
-{
-    node_t *temp = *head;
-
-    if(temp == NULL) 
-    {
-        printf("List is empty! \n");
-    }
-
-    while(temp != NULL) 
-    {
-        printf("| %d | -> ", temp->data);
-        temp = temp->next;
-    }
-    printf("\n");
-}
-
-void printBackward(node_t** head) 
-{
-
-}
-
 void insertBefore(node_t** head, typedef_value_t newValue) 
 {
     node_t* newNode = (node_t*)malloc(sizeof(node_t));
@@ -83,25 +61,129 @@ void insertAfter(node_t** head, typedef_value_t newValue)
     }
 }
 
+void searchNode(node_t** head, typedef_value_t key) 
+{
+    node_t* temp = *head;
+
+    while(temp != NULL) 
+    {
+        if(temp->data == key) 
+        {
+            printf("Node with value %d exist! \n", key);
+            return;
+        }
+            
+        temp = temp->next;
+    }
+    
+    printf("Node with value %d does not exist! \n", key);
+}
+
+void deleteNode(node_t** head, int key) 
+{
+    if(*head == NULL)
+        return;
+    
+    node_t* temp = *head;
+
+    while(temp != NULL && temp->data != key)    // traverse through list
+    {
+        temp = temp->next;
+    }
+
+    if(temp == NULL)            // node does not exist 
+    {
+        printf("Node with value %d does not exist! \n", key);
+    }
+    else if(temp == *head)      // node is the first in the list
+    {
+        *head = (*head)->next;
+        (*head)->prev = NULL;
+        free(temp);
+    }
+    else if(temp->next == NULL) // node is the last one
+    {
+        temp->prev->next = NULL;
+        free(temp);
+    }
+    else                        // node is somewhere between other nodes
+    {
+        temp->prev->next = temp->next;
+        temp->next->prev = temp->prev;
+        free(temp);
+    }
+}
+
+void printBackward(node_t** head) 
+{
+    node_t* temp = *head;
+
+    if(temp == NULL) 
+    {
+        printf("List backward is empty! \n");
+    }
+
+    while(temp->next != NULL) // node is the last one
+    {
+        temp = temp->next;
+    }
+
+    while(temp != NULL) 
+    {
+        printf("| %d | -> ", temp->data);
+        temp = temp->prev;
+    }
+    printf("\n");
+}
+
+void printForward(node_t** head) 
+{
+    node_t *temp = *head;
+
+    if(temp == NULL) 
+    {
+        printf("List forward is empty! \n");
+    }
+
+    
+    printf("Traverse node address: NULL <- ");
+    while(temp != NULL) 
+    {
+        printf("| %d | %d | %d | -> ",temp->prev, &(*temp), temp->next);
+        temp = temp->next;
+    }
+    printf("NULL \n");
+
+    temp = *head;
+    
+    printf("Traverse node value  : NULL <- ");
+    while(temp != NULL) 
+    {
+        printf("| %d |    %d    | %d | -> ",temp->prev, temp->data, temp->next);
+        temp = temp->next;
+    }
+    printf("NULL \n");
+}
+
 int main(void) {
 
     system("clear");
 
-    // insertAfter
-    // insertBefore
+    // createHeadNode();
 
     node_t* origin = (node_t*)malloc(sizeof(node_t));
     origin = NULL;
-    
-    insertAfter(&origin, 5);
-    insertAfter(&origin, 8);
 
     insertBefore(&origin, 10);
-  
+    insertBefore(&origin, 20);
+    insertBefore(&origin, 30);
 
     printForward(&origin);
-    //printBackward(&origin);
-    
+
+    free(origin);
+    origin = NULL;
+    // printForward(&origin);
+
     return 0;
 }
 
